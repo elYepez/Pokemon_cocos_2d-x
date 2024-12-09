689557b31245ecb2908bcb664354ad2892dd8e96
elYepez <yaelyepez2005@gmail.com>
2024-12-08 19:33:37 -0600

    Update PokemonFase0.h

--- PokemonFase0.h
+++ PokemonFase0.h
@@ -5,7 +5,8 @@
 #include <set> // Para manejar las teclas presionadas
 #include "Mapa.h"
 #include "Pokemon.h"
-
+#include <vector>
+#include "AudioManager.h"
 
 USING_NS_CC;
 
@@ -15,32 +16,70 @@ public:
    // Layer* cameraLayer; //La camara que sigue al personaje
 
 
-    // Métodos estáticos
-    static cocos2d::Scene* createScene();
+   
+    static cocos2d::Scene* createScene(Pokemon* pokemon);
 
-    // Inicialización de la escena
-    virtual bool init() override;
+    // Inicializacion de la escena
+    virtual bool init(Pokemon* pokemon);
+    static PokemonFase0* create(Pokemon* pokemon) {
+        PokemonFase0* ret = new (std::nothrow) PokemonFase0();
+        if (ret && ret->init(pokemon)) {
+            ret->autorelease();
+            return ret;
+        }
+        CC_SAFE_DELETE(ret);
+        return nullptr;
+    }
 
-    // Método update llamado en cada frame
+    const cocos2d::Vector<Pokemon*>& getGaleriaPokemones() const { return galeriaPokemones; }
+    void agregarPokemon(Pokemon* pokemon);
+
+    Pokemon* getPokemonSalvajeEnPosicion(int index);//Sistema pokemon salvaje aleatorio
+    int getCantidadPokemonesSalvajes() { return PokemonesSalvajesAleatorios.size(); }
+    void agregarPokemonSalvaje(Pokemon* pokemonSalvaje);
+
+    Pokemon* getPokemonEnPosicion(int index);
+    Pokemon* getPokemonPorNombre(const std::string& nombre);
+    int getCantidadPokemones() { return galeriaPokemones.size(); }
+    void mostrarPokemones();
+    void liberarPokemon(int index);
+    void liberarPokemonPorNombre(const std::string& nombre);
+    void iniciarBatalla(Pokemon* pokemonSalvaje);
+    void capturarPokemon(Pokemon* pokemonSalvaje);
+
+    void detenerPersonajeManual();
+
+    
     void update(float dt);
 
-    // Métodos para manejar las teclas
+    
     void onKeyPressed(EventKeyboard::KeyCode keyCode, Event* event);
     void onKeyReleased(EventKeyboard::KeyCode keyCode, Event* event);
 
-    // Métodos para crear animaciones de caminar
+    
     void crearAnimacionCaminarDerecha();
     void crearAnimacionCaminarIzquierda();
     void crearAnimacionCaminarArriba();
     void crearAnimacionCaminarAbajo();
 
-    // Métodos para inicializar elementos de la escena
+    
     void agregarFondo();
     void inicializarElementos();
     bool puedeMoverse(int x, int y);
     void inicializarPokemones();
     void cambiarMapa(const std::string& direccion);
 private:
+    
+    Pokemon* pokemonInicial;
+   cocos2d::Vector<Pokemon*> galeriaPokemones; //guardar pokemones
+
+   cocos2d::Vector<Pokemon*> PokemonesSalvajesAleatorios; //ruleta de pokemones salvajes
+
+
+   AudioManager* audioManager;
+
+
+    
     // Miembros de la clase
     Sprite* personaje;  // Personaje principal
     std::set<EventKeyboard::KeyCode> keysPressed;  // Conjunto para las teclas presionadas
@@ -60,7 +99,7 @@ private:
     Mapa* mapaActual;
 
     // Implementación de CREATE_FUNC para esta clase
-    CREATE_FUNC(PokemonFase0);
+    //CREATE_FUNC(PokemonFase0);  lo quitamos ya que creamos el nuestro para pasar parametros
 };
 
 #endif // __POKEMON_FASE0_H__
