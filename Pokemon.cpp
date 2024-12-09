7a3865abca2ed5cd1b51af71fcf81233ee7d6094
elYepez <yaelyepez2005@gmail.com>
2024-12-08 19:35:00 -0600

    Update Pokemon.cpp

--- Pokemon.cpp
+++ Pokemon.cpp
@@ -1,10 +1,19 @@
 #include "Pokemon.h"
 
 // Constructor
-Pokemon::Pokemon(std::string& nombre, int nivel, int vidaMaxima, int ataque, int defensa, int velocidad, const std::string& spriteFile, const std::string& descripcion)
+
+
+Pokemon::Pokemon()
+    : nombre(""), nivel(1), vidaMaxima(10), vidaActual(10), ataque(5), defensa(5), velocidad(5), descripcion("") {
+    sprite = nullptr;  
+}
+
+
+Pokemon::Pokemon(const std::string& nombre, int nivel, int vidaMaxima, int ataque, int defensa, int velocidad, const std::string& spriteFile, const std::string& descripcion)
     : nombre(nombre), nivel(nivel), vidaMaxima(vidaMaxima), vidaActual(vidaMaxima), ataque(ataque), defensa(defensa), velocidad(velocidad), descripcion(descripcion) {
 
     sprite = cocos2d::Sprite::create(spriteFile);
+    nombreSprite = spriteFile;
 
     if (nombre == "Bulbasaur") {
         ataques = {
@@ -15,13 +24,37 @@ Pokemon::Pokemon(std::string& nombre, int nivel, int vidaMaxima, int ataque, int
     else if (nombre == "Charmander") {
         ataques = {
             {"Zarpazo",40,100,"Normal"},
-            {"Quemadura",45,100,"Planta"}
+            {"Quemadura",45,100,"Fuego"}
         };
     }
     else if (nombre == "Squirtle") {
         ataques = {
-            {"Madrazo",40,100,"Normal"},
-            {"Water Gun",45,100,"Planta"}
+            {"Moridida",40,100,"Normal"},
+            {"Water Gun",45,100,"Agua"},
+            {"Salpicar",42,100,"Agua"},
+
+        };
+    }
+
+
+    //Pokemones salvajes
+
+    else if (nombre == "Caterpie") {
+        ataques = {
+            {"Picotazo",40,100,"Bicho"},
+            {"Placaje",50,100,"Normal"}
+        };
+    }
+    else if (nombre == "Pidgey") {
+        ataques = {
+            {"Tornado",40,100,"Volador"},
+            {"Ataque Ala",60,100,"Volador"}
+        };
+    }
+    else if (nombre == "Oddish") {
+        ataques = {
+            {"Absorber",20,100,"Planta"},
+            {"Acido",40,100,"Veneno"}
         };
     }
 
@@ -30,7 +63,8 @@ Pokemon::Pokemon(std::string& nombre, int nivel, int vidaMaxima, int ataque, int
 
 Pokemon::~Pokemon() {
     if (sprite) {
-        sprite->release();
+        sprite->autorelease();
+        sprite = nullptr;
     }
 }
 
@@ -69,12 +103,20 @@ int Pokemon::usarMovimiento(Pokemon* enemigo, const std::string& nombreAtaque) {
 
     if (!eleccion) return 0;
 
+    if (rand() % 100 >= eleccion->precision) {
+        return 0;
+    }
+
     float factorNivel = (2.0f * nivel + 10) / 250.0f;
     float factorDefensaEnemigo = float(ataque) / float(enemigo->getDefensa());
     float danioBase = (factorNivel * factorDefensaEnemigo * eleccion->poder + 2);
 
     float multiplicadorRandom = (85 + (rand() % 16)) / 100.0f;
 
-    return static_cast<int>(danioBase * multiplicadorRandom);
+    int danioFinal = static_cast<int>(danioBase * multiplicadorRandom);
+
+    enemigo->recibirDanio(danioFinal);
+
+    return danioFinal;
 
 }
